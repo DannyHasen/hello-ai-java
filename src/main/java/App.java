02c917dbbfdef4aaa9b91dda61fa4b38c41083c2
Danny <hasendanny@gmail.com>
2025-09-03 10:33:15 -0700

    Initial commit: Java fib app

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static int fib(int n) {
+        if (n < 0) throw new IllegalArgumentException("n must be >= 0");
+        if (n <= 1) return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = (args.length > 0) ? Integer.parseInt(args[0]) : 10;
+        System.out.println("fib(" + n + ") = " + fib(n));
+    }
+}
